3c62c4bae35190ccde3c9457ffc7803b12865e6c
Stéphanie Luu <luu.stephanie@gmail.com>
2018-04-08 17:42:32 +0200

    Add assertJ library, add 4 and 5

--- src/RomanConverter.kt
+++ src/RomanConverter.kt
@@ -1,6 +1,13 @@
 class RomanConverter {
 
     fun convert(integer:Int) : String{
-        return "I"
+        return when(integer){
+            1 -> "I"
+            2 -> "II"
+            3 -> "III"
+            4 -> "IV"
+            5 -> "V"
+            else -> "unsupported"
+        }
     }
 }
--- src/RomanConverterTest.kt
+++ src/RomanConverterTest.kt
@@ -1,13 +1,36 @@
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
+import org.assertj.core.api.Assertions.*
 
 class RomanConverterTest {
 
     @Test
     fun `convert 1 return I`(){
         val convertedValue = RomanConverter().convert(1)
+        assertThat(convertedValue).isEqualTo("I")
+    }
 
-        assertEquals("I", convertedValue)
+    @Test
+    fun `convert 2 return II`(){
+        val convertedValue = RomanConverter().convert(2)
+        assertThat(convertedValue).isEqualTo("II")
+    }
+
+    @Test
+    fun `convert 3 return III`(){
+        val convertedValue = RomanConverter().convert(3)
+        assertThat(convertedValue).isEqualTo("III")
+    }
+
+    @Test
+    fun `convert 4 return IV`(){
+        val convertedValue = RomanConverter().convert(4)
+        assertThat(convertedValue).isEqualTo("IV")
+    }
+
+    @Test
+    fun `convert 5 return V`(){
+        val convertedValue = RomanConverter().convert(5)
+        assertThat(convertedValue).isEqualTo("V")
     }
 
 }
