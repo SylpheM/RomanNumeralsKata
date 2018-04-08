9004e75012a75118f51c3122f04946619d9e988a
Stéphanie Luu <luu.stephanie@gmail.com>
2018-04-08 19:26:26 +0200

    Add input to main fun

--- src/app.kt
+++ src/app.kt
@@ -1,6 +1,27 @@
 
 
 fun main(args: Array<String>){
+    println("Hello!")
+    println("Welcome to the converter to roman numerals !")
+    waitInput()
+}
+
+private fun waitInput() {
+    print("Enter a number : ")
+    val input = readLine()
+    if (input == null || input.isBlank()) {
+        println("Please enter something!")
+        waitInput()
+    } else if (input.toIntOrNull() == null){
+        println("Please enter a number!")
+        waitInput()
+    } else {
+        convertInput(input)
+    }
+}
+
+private fun convertInput(input: String) {
     val romanConverter = RomanConverter()
-    println(romanConverter.convert(1))
+    println(romanConverter.convert(input.toInt()))
+    println("Have a nice day =D")
 }
